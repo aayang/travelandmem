dd5521a3c0d1b0a39b9c0fe7370a1f1ef5082eb1
Jason Trieu <j2trieu@ucsd.edu>
2016-11-02 14:44:05 -0700

    change directory structure

---
+++ modules/planner/client/config/planner.client.config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by aayang on 10/26/16.
+ */
+
+'use strict';
+// Configuring the Planner module
+angular.module('planner').run(['Menus',
+    function (Menus) {
+        // Add the articles dropdown item
+        Menus.addMenuItem('topbar', {
+            title: 'Travel Planner',
+            state: 'planner.view'
+        });
+
+        // Add the articles dropdown item
+        Menus.addMenuItem('topbar', {
+            title: 'Extra',
+            state: 'planner.extra'
+        });
+
+        Menus.addMenuItem('topbar', {
+            title: 'Flights',
+            state: 'planner.flights'
+        });
+
+        Menus.addMenuItem('topbar', {
+            title: 'Events',
+            state: 'planner.events'
+        });
+    }
+]);
