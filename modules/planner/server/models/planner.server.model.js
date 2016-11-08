b8923a929bc3627ed508fcd4a3f79ab3b831e29d
Aaron Yang <aaronyang@Aarons-MacBook-Pro.local>
2016-11-07 16:45:21 -0800

    inital css changes

---
+++ modules/planner/server/models/planner.server.model.js
@@ -0,0 +1,19 @@
+/**
+ * Created by aaronyang on 11/7/16.
+ */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var TravelPlan = new Schema({
+
+});
+
+mongoose.model('TravelPlan', TravelPlan);
