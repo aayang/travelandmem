32d90daaabd6617983fd4d33010430c0bd8cdb85
yahav erlich <yahaverlich@gmail.com>
2016-11-07 19:29:34 -0800

    changed server.js to work with MongoLab for Heroku deployment

--- server.js
+++ server.js
@@ -5,3 +5,23 @@
  */
 var app = require('./config/lib/app');
 var server = app.start();
+
+var express = require('express');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var http = require('http');
+var app = express();
+ 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/meals-development');
+app.use(express.static(__dirname + '/build'));
+ 
+app.use(bodyparser.json({limit:'50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
+ 
+require('./routes/admin-routes')(app);
+ 
+var server = http.createServer(app);
+ 
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+console.log("Listening on " + port);
