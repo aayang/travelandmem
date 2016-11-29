de146596d73f3d34189ec8a5ee3148c1d0141aa6
yahav erlich <yahaverlich@gmail.com>
2016-11-28 22:36:42 -0800

    Undid Borken Google analytics stuff, and returned the site back to its working state.

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,27 +5,27 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-    function utmx_section(){}function utmx(){}(function(){var
-k='134756612-0',d=document,l=d.location,c=d.cookie;
-if(l.search.indexOf('utm_expid='+k)>0)return;
-function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
-indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
-length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
-'<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
-'://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
-'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
-valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-'" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
-utmx('url','A/B');
+//     function utmx_section(){}function utmx(){}(function(){var
+// k='134756612-0',d=document,l=d.location,c=d.cookie;
+// if(l.search.indexOf('utm_expid='+k)>0)return;
+// function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
+// indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
+// length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
+// '<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
+// '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
+// '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
+// valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+// '" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
+// utmx('url','A/B');
 
 
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+//       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-88114900-1', 'auto');
-  ga('send', 'pageview');
+//   ga('create', 'UA-88114900-1', 'auto');
+//   ga('send', 'pageview');
 
 
 
